2fed97c39fdd95ae2d9acf8cbbd19d46b657ce80
Abhisek8895 <mishraabhisek14@gmail.com>
2025-09-20 23:02:53 +0530

    Update settings and urls

--- generator/urls.py
+++ generator/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.generate_blog_view, name="generate_blog"),
+    path("", views.blog_generator_view, name="generate_blog"),
 ]
